13f4b5c910e6426ad2bd96161a833704fb3af40c
oguzhandal <daloguz11@hotmail.com>
2021-04-17 03:57:03 +0300

    cache and transaction added

--- Business/Abstract/IProductService.cs
+++ Business/Abstract/IProductService.cs
@@ -14,6 +14,7 @@ namespace Business.Abstract
         IResult Add(Product product);
         IDataResult<Product> GetById(int productId);
         IResult Update(Product product);
+        IResult AddTransactionalTest(Product product);
 
     }
 }
--- Business/Concrete/ProductManeger.cs
+++ Business/Concrete/ProductManeger.cs
@@ -3,6 +3,8 @@ using Business.BusinessAspects.Autofac;
 using Business.CCS;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Caching;
+using Core.Aspects.Autofac.Transaction;
 using Core.Aspects.Autofac.Validation;
 using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Business;
@@ -33,6 +35,8 @@ namespace Business.Concrete
 
         [SecuredOperation("product.add,admin")]
         [ValidationAspect(typeof(ProductValidator))]
+        [CacheRemoveAspect("IProductService.Get")]
+
         public IResult Add(Product product)
         {
             IResult result = BusinessRules.Run(CheckIfProductNameExist(product.ProductName),
@@ -47,6 +51,7 @@ namespace Business.Concrete
             return new SuccessResult(Messages.ProductAdded);
 
         }
+        [CacheAspect]//key,value
         public IDataResult<List<Product>> GetAll()
         {
             //if (DateTime.Now.Hour == 1)
@@ -61,6 +66,7 @@ namespace Business.Concrete
             return new SuccessDataResult<List<Product>>(_productDal.GetAll(item => item.CategoryId == id));
         }
 
+        [CacheAspect]
         public IDataResult<Product> GetById(int productId)
         {
             return new SuccessDataResult<Product>(_productDal.Get(p => p.ProductId == productId));
@@ -77,6 +83,7 @@ namespace Business.Concrete
         }
 
         [ValidationAspect(typeof(ProductValidator))]
+        [CacheRemoveAspect("IProductService.Get")]
         public IResult Update(Product product)
         {
             _productDal.Update(product);
@@ -109,6 +116,13 @@ namespace Business.Concrete
                 return new ErrorResult(Messages.CategoryLimitExceded);
             }
             return new SuccessResult();
+        }
+        [TransactionScopeAspect]
+        public IResult AddTransactionalTest(Product product)
+        {
+            _productDal.Update(product);
+            _productDal.Add(product);
+            return new SuccessResult(Messages.ProductUpdated);
         }
     }
 }
